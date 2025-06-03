d5a9b32d37ab6f2f5e9dd0ee255471802a5dc1bd
Light <speedlightbr@gmail.com>
2025-06-03 20:28:31 -0300

    responsive 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
       <section className="w-full h-screen max-h-[40rem] max-w-[85rem] z-10 flex">
 
-      <div className="flex w-1/2 md:w-[80vh] lg:w-full h-full max-h-[40rem] border-white items-center justify-center ml-10 gap-5 z-10">
+      <div className="flex max-w-[50vh] md:max-w-[80vh] lg:max-w-full h-full w-full max-h-[40rem] border-white items-center justify-center ml-10 gap-5 z-10">
         { /* foto do bot e texto */ }
           <img className="border-2 rounded-full max-w-35 max-h-35 md:max-w-45 md:max-h-45 lg:max-w-70 lg:max-h-70 bg-black border-red-500" src="avatar.png" alt="avatar" />
           <h1 className="w-[30rem] h-[10rem] lg:text-2xl md:text-base text-sm flex font-semibold text-white">👋Olá! Eu sou o Ordo Realitas! Eu sou um bot de rpg de ordem paranormal!
@@ -18,8 +18,8 @@ export default function Home() {
 
         { /* botoes de adicionar e ver ultima atualizacao */ }
           <div className="gap-5 flex flex-col lg:flex-row">
-            <button className="border-2 rounded-md p-2.5 textoResponsivo md:max-h-[20vh] md:max-w-[20vh] lg:max-w-[30vh] max-w-[15vh] text-white bg-purple-500 border-black font-semibold cursor-pointer transition duration-200 hover:bg-purple-700 hover:underline max-h-[8vh]">Adicionar ao servidor</button>
-            <button className="border-2 rounded-md p-2.5 textoResponsivo md:max-h-[20vh] md:max-w-[20vh] lg:max-w-[30vh] max-w-[15vh] text-white bg-red-500 border-black font-semibold cursor-pointer transition duration-200 hover:bg-red-700 hover:underline max-h-[8vh]">Última Atualização</button>
+            <button className="border-2 rounded-md p-2.5 textoResponsivo botaoInicioResponsivo text-white bg-purple-500 border-black font-semibold cursor-pointer transition duration-200 hover:bg-purple-700 hover:underline max-h-[8vh]">Adicionar ao servidor</button>
+            <button className="border-2 rounded-md p-2.5 textoResponsivo botaoInicioResponsivo text-white bg-red-500 border-black font-semibold cursor-pointer transition duration-200 hover:bg-red-700 hover:underline max-h-[8vh]">Última Atualização</button>
           </div>
       </div>
 
