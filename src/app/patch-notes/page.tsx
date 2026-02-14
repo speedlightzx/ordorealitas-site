bdb2604a7f691363e8fa44eebd5c50e745a20452
speedlightzx <speedlightbr@gmail.com>
2026-02-14 12:56:20 -0300

    update patch-notes

--- src/app/patch-notes/page.tsx
+++ src/app/patch-notes/page.tsx
@@ -1,7 +1,7 @@
 export default function patch_notes() {
     return (
         <div className="w-full h-screen bg-neutral-800 p-2 overflow-y-auto ">
-<h1 className="w-full text-center text-[#E0CBFF] underline text-2xl font-semibold">Última atualização: 21/05/2025</h1>
+<h1 className="w-full text-center text-[#E0CBFF] underline text-2xl font-semibold">Última atualização: 12/02/2026</h1>
 
 <div className="border-white flex w-full lg:flex-row flex-col items-center mt-5 gap-y-10">
     <section className="items-center w-1/2 justify-start flex flex-col">
@@ -9,35 +9,21 @@ export default function patch_notes() {
         <div className="overflow-y-auto shadow-md rounded-md border-[#FFD700] border-l-4 w-[35vh] lg:w-full h-screen max-h-[80vh] max-w-[70vh] text-white p-3">
             <p className="whitespace-pre-wrap text-sm">
                 {`
-[+] Adicionado sistema de Trilhas.
-[+] Trilhas adicionadas: 
-      Combatente: Guerreiro e Tropa de Choque.
-      Especialista: Médico de Campo e Atirador de Elite.
-      Ocultista: Conduíte, Flagelador e Lâmina Paranormal.
-[+] Adicionado teste de Reflexos.
-[+] Adicionado teste de Fortitude.
-[+] Novo item: Treino de Medicina: Veterano.
-[+] Agora a perícia de Medicina não é mais completamente inútil.
-[+] Adicionado opção de resetar ficha na criação de uma ficha.
-[~] Os monstros Guardião Ósseo, Escutado, Perturbado de Energia e Anárquico Descontrolado podem fazer o jogador passar por um teste de resistência.
-
-[+] Novo ritual: Hemofagia ★★.
-[+] Novo ritual: Proteção contra Rituais ★★.
-[+] Novo ritual: Rejeitar Névoa ★★.
-[+] Novo ritual: Aprimorar Mente ★★.
-[+] Novo ritual: Arma Atroz ★.
-[+] Novo ritual: Paradoxo ★★.
-[+] Novo ritual: Espirais da Perdição ★.
-[+] Novo poder paranormal: Potencial Reaproveitado.
-[+] Novo poder paranormal: Precognição.
-[+] Novo item: Treino de Reflexos: Veterano.
-
-[+] Adicionado monstros com chance de spawn que são mais fortes do que o comum...
-[+] Nex máximo agora é 75%.
-[+] Novas dungeons adicionadas.
-[+] Novos monstros adicionados.
-[+] Novas missões adicionadas.
-[+] Novos itens únicos de monstros adicionados.
+[+] Novo botão adicionado nos combates: Habilidades.
+[+] Agora é possível stackar itens utilizáveis.
+[+] Adicionado itens amaldiçoados(armas e armaduras).
+[+] Novos pergaminhos amaldiçoados: Pergaminho Amaldiçoador de Armas e Pergaminho Amaldiçoador de Proteções.
+[+] Novo item: Bandagem.
+[+] Novo item: Kit Médico.
+[+] Maldições de armas adicionadas: Antielemento, Senciente, Repulsora, Energética, Erosiva, Lancinante e Sanguinária.
+[+] Maldições de proteções adicionadas: Abascanta, Cinética, Voltaica, Repulsiva, Regenerativa.
+[+] Novo equipável: Escudo.
+[+] Novo ritual: Armadura de Sangue ★.
+[+] Novo ritual: Fortalecimento Sensorial ★.
+[+] Novo ritual: Descarnar ★★★.
+[+] Novo ritual: Ferver Sangue ★★★.
+[+] Novo ritual: Troca Justa ★★.
+[+] Novo comando: /ajuda.
                 `}
             </p>
         </div>
@@ -48,23 +34,22 @@ export default function patch_notes() {
                 <div className="overflow-y-auto shadow-md rounded-md border-[#FFD700] border-l-4 w-[35vh] lg:w-full h-screen max-h-[80vh] max-w-[70vh] text-white p-3">
             <p className="whitespace-pre-wrap text-sm">
                 {`
-[~] As missões agora mostram o progresso.
-[~] A opção de bloquear agora só aparece quando tem algum bônus de fortitude.
-[+] Dama de Sangue agora pode dropar Empaladora Sangrenta.
-[+] Lembrado agora pode dropar Hive Mind.
-[+] Aumentado o dano do Deflagração de Energia: 3d10+10->4d10+10.
-[+] Aumentado o dano normal e crítico de todos os rituais de amaldiçoar arma com elemento: 1d6->1d8, 3d6->3d8.
-[+] Sniper agora tem mais chance de dar crítico.
-[-] Bloquear agora não usa mais vigor para somar no dano reduzido ( exceto com casca grossa ).
-[-] Aumentado levemente a vida dos monstros das dungeon tier 1 e 2.
-[-] Diminuído o dano de perfuração do Guardião Ósseo.
-[-] Aumentado levemente o dano de sanidade do Perturbado de Energia.
-[-] Aumentado levemente a defesa de alguns monstros.
-[-] Aumentado o preço da Katana Desfragmentada e Tridente para 6500.
-[-] Diminuído o dano do Flecha Flamejante: 5d10+10->5d10, 9d8+5->8d8.
-[-] Aumentado o custo de PE do Deflagração de Energia: 6->8.
-[-] Aumentado o custo de PE do Aprimorar Físico: 4->8.
-[-] Treino de Pontaria: Veterano e Treino de Luta: Veterano não aparecem mais no /loja.
+[~] Alterado as recompensas do /vote: Lâmina Femoral,  Mini Gun, Pergaminho1, Pergaminho2, Pergaminho Amaldiçoador de Armas e Pergaminho Amaldiçoador de Proteções
+[~] Alterado o visual de alguns comandos.
+[~] Agora os rituais são categorizados pelo elemento escolhido.
+[~] Agora o revidar pode contra atacar dando dano elemental.
+[~] Agora o /eitem mostra a raridade dos itens e um emoji de acordo com o tipo do item.
+[~] Agora o /roll também mostra a rolagem de dados feita.
+[~] Agora o /missoes mostra quanto tempo falta para resetar as missões.
+[~] Aumentado a chance de spawn do Cultista Lunático e Enpap-X.
+[+] Aumentado a chance de drop do Pergaminho3 na dungeon dos cultistas e na dungeon tier 3.
+[+] Aumentado a chance de drop dos treinos veteranos de perícia na dungeon dos cultistas e na dungeon tier 3.
+[+] Aumentado a chance de drop da Corrente do Condenado Eterno.
+[+] Aumentada a vida do ritual de Tela de Ruído: 40->50.
+[-] Aumentado o dano de um dos ataques do Enpap-X.
+[-] Aumentado a DT dos testes de fortitude do Enpap-X: 15->20.
+[-] Reduzido o buff de dano do Enpap-X: 10->1.
+[-] Reduzido o dano de todos os ataques da Múmia Xipófaga.
                 `}
             </p>
         </div>
